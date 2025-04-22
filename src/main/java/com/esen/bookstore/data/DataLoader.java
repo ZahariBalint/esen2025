b150628b6ac4fe78d8f35dfe0e88e26ff5943982
jooszebi26 <jooszebi26@gmail.com>
2025-04-22 09:33:49 +0200

    Added interactive CLI

--- src/main/java/com/esen/bookstore/data/DataLoader.java
+++ src/main/java/com/esen/bookstore/data/DataLoader.java
@@ -1,11 +1,13 @@
 package com.esen.bookstore.data;
 
 import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.Bookstore;
 import com.esen.bookstore.repository.BookRepository;
 import com.esen.bookstore.repository.BookStoreRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -15,18 +17,17 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class DataLoader {
 
     private final BookRepository bookRepository;
     private final BookStoreRepository bookstoreRepository;
 
-    public DataLoader(BookRepository bookRepository, BookStoreRepository bookStoreRepository) {
-        this.bookRepository = bookRepository;
-        this.bookstoreRepository = bookStoreRepository;
-    }
 
     @Value("classpath:data/books.json")
     private Resource booksResource;
@@ -41,6 +42,19 @@ public class DataLoader {
             var booksJson = StreamUtils.copyToString(booksResource.getInputStream(), StandardCharsets.UTF_8);
             var books = objectMapper.readValue(booksJson, new TypeReference<List<Book>>(){});
             bookRepository.saveAll(books);
+
+            var bookstoresJson = StreamUtils.copyToString(bookstoresResource.getInputStream(),StandardCharsets.UTF_8);
+            var bookstores = objectMapper.readValue(bookstoresJson, new TypeReference<List<Bookstore>>() {});
+
+            bookstores.forEach(bookstore -> {
+                bookstore.setInventory(books.stream()
+                        .collect(Collectors.toMap(
+                                book -> book,
+                                book -> ThreadLocalRandom.current().nextInt(1,50)
+                        )));
+            });
+
+            bookstoreRepository.saveAll(bookstores);
         } catch (IOException e) {
             System.out.printf("Cannot seed database ",e);
         }
